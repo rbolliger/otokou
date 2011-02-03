19a158645433d29fa3b170bef425de7ccb8135de
Raffaele Bolliger <raffaele.bolliger@gmail.com>
2011-02-03 15:47:43 +0100

    - Added menu item allowing the suer to change his password.

--- website/apps/frontend/modules/user_settings/templates/_user_settings_menu.php
+++ website/apps/frontend/modules/user_settings/templates/_user_settings_menu.php
@@ -7,6 +7,7 @@
 
 $menu = new ioMenu(array('id' => 'user_settigns_menu'));
 $menu->addChild('Account','@user_settings_account');
+$menu->addChild('Change password','@sf_guard_forgot_password');
 $menu->addChild('Charges list', '@user_settings_otokou');
 echo $menu->render();
 
