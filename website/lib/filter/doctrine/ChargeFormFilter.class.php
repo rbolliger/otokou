252aedaaf83872ace1ff5857b0b52bbf70145b8d
Raffaele Bolliger <raffaele.bolliger@gmail.com>
2011-02-10 12:24:01 +0100

    Refactored filters. Moved some code from extended to base filter.

--- website/lib/filter/doctrine/ChargeFormFilter.class.php
+++ website/lib/filter/doctrine/ChargeFormFilter.class.php
@@ -12,5 +12,85 @@ class ChargeFormFilter extends BaseChargeFormFilter
 {
   public function configure()
   {
+      parent::configure();
+      
+             
+        $this->widgetSchema['category_id']->setOption('expanded', true);
+        $this->widgetSchema['category_id']->setOption('multiple', true);
+        $this->widgetSchema['category_id']->setOption('add_empty', false);
+        
+        $this->validatorSchema['category_id']->setOption('multiple', true);
+
+
+
+        $widget = new sfWidgetFormDate(array(
+                    'format' => '%day%/%month%/%year%',
+                ));
+
+        $this->widgetSchema['date'] = new sfWidgetFormFilterDate(array(
+                    'from_date' => new sfWidgetFormJQueryDate(array(
+                        'config' => '{}',
+                        'image' => '/images/calendar.png',
+                        'date_widget' => $widget
+                    )),
+                    'to_date' => new sfWidgetFormJQueryDate(array(
+                        'config' => '{}',
+                        'image' => '/images/calendar.png',
+                        'date_widget' => $widget
+                    )),
+                    'with_empty' => false,
+                    'template' => 'from %from_date% to %to_date%',
+                ))
+        ;
+
+        $this->widgetSchema['kilometers'] = new sfWidgetFormFilterDate(array(
+                    'from_date' => new sfWidgetFormInput(array('default' => null)),
+                    'to_date' => new sfWidgetFormInput(array('default' => null)),
+                    'with_empty' => false,
+                    'template' => 'between %from_date% and %to_date%',
+                ));
+
+        $this->validatorSchema['kilometers'] = new sfValidatorDateRange(array(
+                    'required' => false,
+                    'from_date' => new sfValidatorNumber(array('required' => false)),
+                    'to_date' => new sfValidatorNumber(array('required' => false))
+                ));
+
+
+        $this->widgetSchema['amount'] = clone $this->widgetSchema['kilometers'];
+        $this->validatorSchema['amount'] = clone $this->validatorSchema['kilometers'];
+        
+        $this->widgetSchema['quantity'] = clone $this->widgetSchema['kilometers'];
+        $this->validatorSchema['quantity'] = clone $this->validatorSchema['kilometers'];
+        
+      
   }
+  
+  
+      public function addKilometersColumnQuery(Doctrine_Query $query, $field, $values) {
+        $this->addRangeQuery($query, $field, $values);
+    }
+
+    public function addAmountColumnQuery(Doctrine_Query $query, $field, $values) {
+
+        $this->addRangeQuery($query, $field, $values);
+    }
+    
+    public function addQuantityColumnQuery(Doctrine_Query $query, $field, $values) {
+
+        $this->addRangeQuery($query, $field, $values);
+    }
+
+    public function addRangeQuery(Doctrine_Query $query, $field, $values) {
+        
+        $fieldName = $this->getFieldName($field);
+
+        if ($values['from']) {
+            $query->andWhere(sprintf('%s.%s >= ?', $query->getRootAlias(), $fieldName), $values['from']);
+        }
+        
+        if ($values['to']) {
+            $query->andWhere(sprintf('%s.%s <= ?', $query->getRootAlias(), $fieldName), $values['to']);
+        }
+    }
 }
--- website/lib/filter/doctrine/ChargeFormWithUserFilter.class.php
+++ website/lib/filter/doctrine/ChargeFormWithUserFilter.class.php
@@ -8,110 +8,38 @@
  * @author     Raffaele Bolliger
  * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class ChargeFormWithUserFilter extends BaseChargeFormFilter {
+class ChargeFormWithUserFilter extends ChargeFormFilter {
 
     public function configure() {
 
+        parent::configure();
+        
+        
+        unset($this['user_id']);
+
         $q = Doctrine_Core::getTable($this->getRelatedModelName('Vehicle'))
-                ->createQuery('v')
-                ->andWhere('v.user_id = ?', $this->getUserId())
-                ->andWhere('v.is_archived = ?',false);
+                        ->createQuery('v')
+                        ->andWhere('v.user_id = ?', $this->getUserId())
+                        ->andWhere('v.is_archived = ?', false);
 
         $this->widgetSchema['vehicle_id'] = new sfWidgetFormDoctrineChoice(array(
-                    'model'     => $this->getRelatedModelName('Vehicle'),
-                    'query'     => $q,
-                    'multiple'  => true,
-                    'expanded'  => true,
+                    'model' => $this->getRelatedModelName('Vehicle'),
+                    'query' => $q,
+                    'multiple' => true,
+                    'expanded' => true,
                 ));
 
         $this->validatorSchema['vehicle_id'] = new sfValidatorDoctrineChoice(array(
                     'model' => $this->getRelatedModelName('Vehicle'),
                     'query' => $q,
                     'required' => false,
-                    'multiple'  => true,
+                    'multiple' => true,
                 ));
-
-        
-        $this->widgetSchema['category_id']->setOption('expanded', true);
-        $this->widgetSchema['category_id']->setOption('multiple', true);
-        $this->widgetSchema['category_id']->setOption('add_empty', false);
-        
-        $this->validatorSchema['category_id']->setOption('multiple', true);
-
-
-
-        $widget = new sfWidgetFormDate(array(
-                    'format' => '%day%/%month%/%year%',
-                ));
-
-        $this->widgetSchema['date'] = new sfWidgetFormFilterDate(array(
-                    'from_date' => new sfWidgetFormJQueryDate(array(
-                        'config' => '{}',
-                        'image' => '/images/calendar.png',
-                        'date_widget' => $widget
-                    )),
-                    'to_date' => new sfWidgetFormJQueryDate(array(
-                        'config' => '{}',
-                        'image' => '/images/calendar.png',
-                        'date_widget' => $widget
-                    )),
-                    'with_empty' => false,
-                    'template' => 'from %from_date% to %to_date%',
-                ))
-        ;
-
-        $this->widgetSchema['kilometers'] = new sfWidgetFormFilterDate(array(
-                    'from_date' => new sfWidgetFormInput(array('default' => null)),
-                    'to_date' => new sfWidgetFormInput(array('default' => null)),
-                    'with_empty' => false,
-                    'template' => 'between %from_date% and %to_date%',
-                ));
-
-        $this->validatorSchema['kilometers'] = new sfValidatorDateRange(array(
-                    'required' => false,
-                    'from_date' => new sfValidatorNumber(array('required' => false)),
-                    'to_date' => new sfValidatorNumber(array('required' => false))
-                ));
-
-
-        $this->widgetSchema['amount'] = clone $this->widgetSchema['kilometers'];
-        $this->validatorSchema['amount'] = clone $this->validatorSchema['kilometers'];
-        
-        $this->widgetSchema['quantity'] = clone $this->widgetSchema['kilometers'];
-        $this->validatorSchema['quantity'] = clone $this->validatorSchema['kilometers'];
-        
     }
 
     protected function getUserId() {
 
         return sfContext::getInstance()->getUser()->getGuarduser()->getId();
-    }
-
-    public function addKilometersColumnQuery(Doctrine_Query $query, $field, $values) {
-        $this->addRangeQuery($query, $field, $values);
-    }
-
-    public function addAmountColumnQuery(Doctrine_Query $query, $field, $values) {
-
-        $this->addRangeQuery($query, $field, $values);
-    }
-    
-    public function addQuantityColumnQuery(Doctrine_Query $query, $field, $values) {
-
-        $this->addRangeQuery($query, $field, $values);
-    }
-
-    public function addRangeQuery(Doctrine_Query $query, $field, $values) {
-        
-        $fieldName = $this->getFieldName($field);
-
-        if ($values['from']) {
-            $query->andWhere(sprintf('%s.%s >= ?', $query->getRootAlias(), $fieldName), $values['from']);
-        }
-        
-        if ($values['to']) {
-            $query->andWhere(sprintf('%s.%s <= ?', $query->getRootAlias(), $fieldName), $values['to']);
-        }
     }
 
 }
