a5cd597a24aadc8cd7ce92adb9ca447b43f6012f
Raffaele Bolliger <raffaele.bolliger@gmail.com>
2011-02-10 09:16:49 +0100

    - Generated graphsmodule - Created a new route in routing.yml - Created a new routing class based on sfRequestRoute

---
+++ website/lib/routing/otkWithUserRoute.class.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of otkWithUserRoute
+ *
+ * @author Raffaele Bolliger <raffaele.bolliger at gmail.com>
+ */
+class otkWithUserRoute extends sfRequestRoute {
+
+    public function generate($params, $context = array(), $absolute = false) {
+
+
+        $params = array_merge(array('username' => $this->getUserId()), $params);
+
+
+        return parent::generate($params, $context, $absolute);
+    }
+
+    protected function getUserId() {
+        $user = sfContext::getInstance()->getUser();
+
+        return $user->isAuthenticated() ? $user->getGuardUser()->getUsername() : 0;
+    }
+
+
+    public function matchesParameters($params, $context = array()) {
+
+        $params = array_merge(array('username' => $this->getUserId()), $params);
+
+        return parent::matchesParameters($params, $context);
+    }
+
+}
+
