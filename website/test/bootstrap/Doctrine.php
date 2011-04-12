2179378e911d72bbce17a3add7939d5dd6c3abd0
Raffaele Bolliger <raffaele.bolliger@gmail.com>
2011-04-12 22:20:11 +0200

    Removed unused commented lines

--- website/test/bootstrap/Doctrine.php
+++ website/test/bootstrap/Doctrine.php
@@ -4,7 +4,5 @@ include(dirname(__FILE__).'/unit.php');
 $configuration = ProjectConfiguration::getApplicationConfiguration( 'frontend', 'test', true);
 new sfDatabaseManager($configuration);
 
-//Doctrine_Manager::getInstance()->createDatabases('doctrine');
-
 Doctrine::createTablesFromModels(dirname(__FILE__).'/../../lib/model');
 Doctrine::loadData(sfConfig::get('sf_test_dir').'/fixtures');
