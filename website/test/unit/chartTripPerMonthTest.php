e675b07a006196e219629f47a1df624abb06922e
Raffaele Bolliger <raffaele.bolliger@gmail.com>
2011-04-17 15:03:47 +0200

    Updated tests

--- website/test/unit/chartTripPerMonthTest.php
+++ website/test/unit/chartTripPerMonthTest.php
@@ -6,7 +6,7 @@ include dirname(__FILE__) . '/../bootstrap/Doctrine.php';
 
 $ut = new chartSourceUtilityTest(new sfBrowser());
 
-$t = new lime_test(64, new lime_output_color());
+$t = new lime_test(128, new lime_output_color());
 
 
 $scenarios = $ut->getBaseScenarios();
@@ -14,19 +14,25 @@ $scenarios = $ut->getBaseScenarios();
 
 $options = 'month';
 
+$params = array(
+    'full_history' => true, // this is to define if data must be recovered from the beginning (by ignoring start_limit) or not
+);
 
-foreach ($scenarios as $key => $scenario) {
+//foreach ($scenarios as $key => $scenario) {
+for ($index = 0; $index < 16; $index++) {
+    $scenario = $scenarios[$index];
 
     $x = getXForScenario($ut, $scenario);
     $y = getYForScenario($ut, $scenario);
 
-    $ut->runTest($t, $scenario, 'buildTripChartData', $x, $y, $options);
+    $ut->runTest($t, $scenario, 'buildTripChartData', $x, $y, $options, $params);
 }
 
 function getYForScenario($ut, $scenario) {
 
     $case = $ut->getCase($scenario[0], $scenario[1]);
     $range = $scenario[2];
+    $limit = isset($scenario[3]) ? true : false;
 
     switch ($case) {
         case 2:
@@ -37,17 +43,22 @@ function getYForScenario($ut, $scenario) {
         case 1:
 
             $y = array(
-                0 => array(-88,111,333,-406,20,330),
+                0 => array(-88, 111, 333, -406, 20, 330),
             );
             break;
 
         case 3:
 
             $y = array(
-                0 => array(0,100,100,-250,20,330),
-                1 => array(-312,111,333,0,0,0),
+                0 => array(0, 100, 100, -250, 20, 330),
+                1 => array(-312, 111, 333, 0, 0, 0),
             );
 
+            if ($limit && 'date' == $range) {
+                foreach ($y as $key => $serie) {
+                    $y[$key] = array_slice($serie, 0, 4);
+                }
+            }
             break;
 
 
@@ -61,6 +72,10 @@ function getYForScenario($ut, $scenario) {
 
 function getXForScenario($ut, $scenario) {
 
+    $case = $ut->getCase($scenario[0], $scenario[1]);
+    $range = $scenario[2];
+    $limit = isset($scenario[3]) ? true : false;
+
     $x = array(
         '2011-Jan',
         '2011-Feb',
@@ -69,6 +84,10 @@ function getXForScenario($ut, $scenario) {
         '2011-May',
         '2011-Jun',
     );
+
+    if ($case == 3 && $limit && 'date' == $range) {
+        $x = array_slice($x, 0, 4);
+    }
 
     return $x;
 }
