65d5374616d0ee5ee837e8d7f170c629162b274b
Raffaele Bolliger <raffaele.bolliger@gmail.com>
2011-04-28 08:09:56 +0200

    Improved menu

--- website/apps/frontend/modules/report/templates/_reportsMenu.php
+++ website/apps/frontend/modules/report/templates/_reportsMenu.php
@@ -10,4 +10,13 @@ foreach ($vehicles as $vehicle) {
 } ?>
 
 
-<?php echo $menu->render(); ?> 
+<?php echo $menu->render(); ?>
+
+<h2>Custom reports</h2>
+
+<?php $menu2 = new ioMenu(array('class' => 'custom_reports_menu')); ?>
+
+
+<?php $menu2->addChild('Custom reports', '@reports_custom_list',array('class' => 'vehicle_active')); ?>
+
+<?php echo $menu2->render(); ?>
