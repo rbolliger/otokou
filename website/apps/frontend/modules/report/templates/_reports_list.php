7b60402a65f8151c2aee11bc9d0d898387c5a4c5
Raffaele Bolliger <raffaele.bolliger@gmail.com>
2011-04-28 10:12:44 +0200

    Improved reports listing

--- website/apps/frontend/modules/report/templates/_reports_list.php
+++ website/apps/frontend/modules/report/templates/_reports_list.php
@@ -1,10 +1,58 @@
-<ul class="reports_list">
-    <?php foreach ($reports as $report) : ?>
-        <li class="<?php echo $report->getIsNew() ? 'report_new' : 'report_old'; ?>">
-        <?php
-        echo link_to($report->getName(), '@report_show?slug=' . $report->getSlug());
-        echo link_to(image_tag('pdf_icon'), '@report_pdf?slug=' . $report->getSlug())
-        ?>
-    </li>
-    <?php endforeach; ?>
-</ul>
+<table class="reports_list">
+    <thead>
+        <tr>
+            <td>new?</td>
+            <td>Pdf</td>
+            <td>Name</td>
+            <td>From</td>
+            <td>To</td>
+            <td>Vehicles</td>
+
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($reports as $report) : ?>
+            <tr class="<?php echo $report->getIsNew() ? 'report_new' : 'report_old'?>">
+                <td><?php echo $report->getIsNew() ? image_tag('icons/star') : '&nbsp;' ?></td>
+                <td><?php echo link_to(image_tag('icons/page_white_acrobat'), '@report_pdf?slug=' . $report->getSlug()) ?></td>
+                <td><?php echo link_to($report->getName(), '@report_show?slug=' . $report->getSlug()); ?></td>
+                <td>
+                <?php
+                if ($report->getDateFrom()) {
+                    $from = $report->getDateFrom();
+                } elseif ($report->getKilometersFrom()) {
+                    $from = $report->getKilometersFrom() . ' km';
+                } else {
+                    $from = '&nbsp;';
+                }
+                echo $from;
+                ?>
+            </td>
+            <td>
+                <?php
+                if ($report->getDateTo()) {
+                    echo $report->getDateTo();
+                } elseif ($report->getKilometersTo()) {
+                    echo $report->getKilometersTo() . ' km';
+                } else {
+                    echo '&nbsp;';
+                }
+                ?>
+            </td>
+            <td>
+                <?php
+                $vehicles = $report->getVehicles();
+                $nv = count($vehicles);
+                foreach ($vehicles as $key => $v) {
+                    echo $v->getName();
+                    if ($key < $nv - 1) {
+                        echo ', ';
+                    }
+                }
+                ?>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
