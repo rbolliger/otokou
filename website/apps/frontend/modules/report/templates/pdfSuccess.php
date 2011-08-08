75553b5a633faa5333ce7f355fc90bb365cc0827
Raffaele Bolliger <raffaele.bolliger@gmail.com>
2011-08-08 21:13:29 +0200

    forgot file in previous commit

---
+++ website/apps/frontend/modules/report/templates/pdfSuccess.php
@@ -0,0 +1,9 @@
+<?php
+slot('leftcol');
+include_component('report', 'reportsMenu');
+end_slot();
+?>
+
+<h1>Sorry</h1>
+
+Cannot create a Pdf report since there are no charges available for the requested date and/or kilometers range.
