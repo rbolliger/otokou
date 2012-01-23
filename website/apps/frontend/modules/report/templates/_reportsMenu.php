dc9d3188eeb4cc02a25259dc07ff3606898b4b22
Raffaele Bolliger <raffaele.bolliger@gmail.com>
2012-01-23 22:57:50 +0100

    Improved multi-level menus css

--- website/apps/frontend/modules/report/templates/_reportsMenu.php
+++ website/apps/frontend/modules/report/templates/_reportsMenu.php
@@ -1,26 +1,14 @@
-<h2>Vehicles</h2>
 
-<?php $menu = new ioMenu(array('class' => 'vehicles_menu')); ?>
+<?php $menu = new ioMenu(array('id' => 'vehicles_menu', 'class' => 'vert-menu')); 
 
-<?php
+$menu->addChild('By Vehicle','@report_index');
 foreach ($vehicles as $vehicle) {
 
     $class = $vehicle->getIsArchived() ? 'vehicle_archived' : 'vehicle_active';
-    $menu->addChild($vehicle->getName(), '@reports_list_vehicle?slug=' . $vehicle->getSlug(),array('class' => $class));
-} ?>
+    $menu['By Vehicle']->addChild($vehicle->getName(), '@reports_list_vehicle?slug=' . $vehicle->getSlug(),array('class' => $class));
+} 
 
-
-<?php echo $menu->render(); ?>
-
-<h2>Custom reports</h2>
-
-<?php $menu2 = new ioMenu(array('class' => 'custom_reports_menu')); ?>
-<?php $menu2->addChild('Custom reports', '@reports_list_custom',array('class' => 'vehicle_active')); ?>
-<?php echo $menu2->render(); ?>
-
-
-<h2>Create a new report</h2>
-
-<?php $menu3 = new ioMenu(array('class' => 'report_new')); ?>
-<?php $menu3->addChild('New report', '@report_new',array('class' => 'vehicle_active')); ?>
-<?php echo $menu3->render(); ?>
+$menu->addChild('Custom reports', '@reports_list_custom',array('class' => 'vehicle_active'));
+$menu->addChild('Create a new report', '@report_new',array('class' => 'vehicle_active')); 
+echo $menu->render();
+?>
