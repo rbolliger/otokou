4172765f89b5add5d18b06acdbe52d7334c5174d
Raffaele Bolliger <raffaele.bolliger@gmail.com>
2012-01-24 16:24:15 +0100

    Improved signin template css

--- website/apps/frontend/modules/sfGuardAuth/templates/signinSuccess.php
+++ website/apps/frontend/modules/sfGuardAuth/templates/signinSuccess.php
@@ -1,7 +1,15 @@
 <?php use_helper('I18N') ?>
 
+<?php slot('content_title') ?>
 <h1><?php echo __('Signin', null, 'sf_guard') ?></h1>
+<?php end_slot(); ?>
+
+
+<div class="bigbox">
+    <p>The page you are requesting requires a valid account.</p>
 
 <div class="signin_form">
     <?php echo get_partial('sfGuardAuth/signin_form', array('form' => $form)) ?>
+</div>
+
 </div>
