59c1dd29bee76f7e8a7f5f419168a65ffcce7003
Raffaele Bolliger <raffaele.bolliger@gmail.com>
2012-01-26 17:15:30 +0100

    Improved css

--- website/apps/frontend/modules/report/templates/_reports_list_vehicle.php
+++ website/apps/frontend/modules/report/templates/_reports_list_vehicle.php
@@ -3,14 +3,24 @@
 
 
     <div class="reports_category" id="<?php echo 'category_'.$category->getSlug() ?>">
-        <h2 class="report_category_title"><?php
+        <h3 class="report_category_title"><?php
     echo link_to(
             $category->getName(), '@reports_list_vehicle?slug=' . $category->getSlug()
     )
-    ?></h2>
+    ?></h3>
 
 
         <table class="reports_list">
+            <thead>
+                <tr>
+                    <th>Is new?</th>
+                    <th>Name</th>
+                    <th>From</th>
+                    <th>To</th>
+                    <th>Vehicles</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
             <tbody>
                 <?php foreach ($category->getOwnReports(sfConfig::get('app_report_max_on_index')) as $i => $report): ?>
                     <?php include_partial('report/report_properties', array('report' => $report)); ?>
