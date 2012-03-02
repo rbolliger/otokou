eb64b50fcdf7aa983ddd576d460485f9204829d0
dbergomi <davebergomi@hotmail.com>
2012-03-02 11:44:54 +0100

    Testing encryption for api using openssl rsa

--- website/apps/frontend/modules/api/templates/getUserSuccess.php
+++ website/apps/frontend/modules/api/templates/getUserSuccess.php
@@ -1,3 +1,28 @@
 <?php
 	echo htmlspecialchars_decode($api->getResponse());
+	
+	/*
+	$os = new apiOpenSsl();
+	$os->createKeys();
+	echo "<br/>priv: ".$os->privKey."<br/>";
+	echo "<br/>pub: ".$os->pubKey["key"]."<br/>";
+	$source= "some text";
+	echo "<br/>original: ".$source."<br/>";
+	$crypttext = $os->encrypt($source);
+	echo "<br/>encrypt: ".$crypttext."<br/>";
+	$newsource = $os->decrypt($crypttext);
+	echo "<br/>decrypt: ".$newsource."<br/>";
+	
+	$handle = fopen("./pdf/prkey.txt", "w");
+	fwrite($handle,$os->privKey);
+	fclose($handle);
+	
+	$handle = fopen("./pdf/pukey.txt", "w");
+	fwrite($handle,$os->pubKey["key"]);
+	fclose($handle);
+	
+	$handle = fopen("./pdf/crypt.txt", "w");
+	fwrite($handle,$crypttext);
+	fclose($handle);
+	*/
 ?>
